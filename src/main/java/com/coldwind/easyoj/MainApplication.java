b8624a9e232dbfd4968e88587e628ee2bccfe9a2
ckl <835094257@qq.com>
2024-01-12 18:33:29 +0800

    项目精简

--- src/main/java/com/coldwind/easyoj/MainApplication.java
+++ src/main/java/com/coldwind/easyoj/MainApplication.java
@@ -3,6 +3,7 @@ package com.coldwind.easyoj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,8 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * EL PSY KONGROO
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
-    // (exclude = {RedisAutoConfiguration.class})
-@SpringBootApplication
+//     (exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 @MapperScan("com.coldwind.easyoj.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
